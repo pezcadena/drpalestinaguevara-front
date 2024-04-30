b685cae07a1e0b65af1da360c3dcaec1cf1bf7e7
Angel Cadena <angellopezcadena@gmail.com>
2024-04-30 11:30:54 -0600

    test(Landing): add publication route

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,6 +1,7 @@
 import { PageDescriptionSlice, TitleSlice } from "../../../prismicio-types";
 import Gallery from "./components/gallery";
 import HeadlineCard from "./components/headline-card";
+import NewContentWrapper from "./components/new-content-wrapper";
 import Subtitle from "./components/subtitle";
 import {createClient} from "@/prismicio";
 import { LangProps } from "./publications/page";
@@ -19,14 +20,14 @@ export default async function Home({params:{lang}}:LangProps) {
   const images = page.data.slices[0]?.items;
   
 
-  /* const recentPublications = await client.getAllByTag('Content',{
+  const recentPublications = await client.getAllByTag('Content',{
     orderings:[
       {
         field:'my.publication.date',
         direction:'desc'
       }
     ],
-  }); */
+  });
 
   return (
     <div className="flex flex-col gap-gap">
@@ -52,7 +53,7 @@ export default async function Home({params:{lang}}:LangProps) {
           }
         </div>
         <Subtitle label={langDictionary.lastPublicationsTitle}/>
-        {/* <div className="flex flex-wrap -m-mitad-gap">
+        <div className="flex flex-wrap -m-mitad-gap">
           {
             recentPublications.map(document=>
               <div 
@@ -65,7 +66,7 @@ export default async function Home({params:{lang}}:LangProps) {
               </div>
             )
           }
-        </div> */}
+        </div>
       </section>
     </div>
   );
