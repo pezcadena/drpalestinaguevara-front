96b2433717028652d8365522add6404a48ed3d1e
Angel Cadena <angellopezcadena@gmail.com>
2024-04-30 18:23:49 -0600

    feat(Gallery): add swiper

--- src/app/[lang]/components/gallery.tsx
+++ src/app/[lang]/components/gallery.tsx
@@ -1,6 +1,12 @@
+"use client"
 import Image from "next/image"
 import { Simplify, TitleSliceDefaultItem } from "../../../../prismicio-types"
 import { PrismicNextImage } from "@prismicio/next"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Pagination, Autoplay } from 'swiper/modules';
 
 interface GalleryProps{
     images:never[] | Simplify<TitleSliceDefaultItem>[] | undefined
@@ -9,7 +15,47 @@ export default function Gallery({
     images
 }:GalleryProps){
     return (
-        <div className="flex gap-gap overflow-hidden w-full lg:min-w-[778px] rounded">
+        <div className="w-full lg:min-w-[778px] rounded">
+            <Swiper
+                modules={[Pagination, Autoplay]}
+                spaceBetween={0}
+                pagination={{ 
+                    clickable: true,
+                    bulletActiveClass:'bg-primary opacity-100',
+                }}
+                className="w-full rounded"
+                autoplay={{
+                    delay:4000,
+                    pauseOnMouseEnter:true,
+                }}
+                loop={true}
+                slidesPerView={'auto'}
+            >
+                {
+                    images?.map(image=>
+                        <SwiperSlide
+                        key={image.photo.id} 
+                        className="lg:max-w-[678px] mr-padding lg:mr-gap"
+                        >
+                            <section
+                                className="rounded h-[433px] flex justify-center overflow-hidden w-full relative items-center min-w-[395px] lg:max-w-[678px]"
+                            >
+                                <PrismicNextImage
+                                    field={image.photo} 
+                                    width={1000}
+                                    height={1000}
+                                    className="w-full h-full object-cover absolute"
+                                />
+                            </section>
+                        </SwiperSlide>
+                    )
+                }
+            </Swiper>
+        </div>
+    )
+}
+
+/* <div className="flex gap-gap overflow-hidden w-full lg:min-w-[778px] rounded">
             {
                 images?.map(image=>
                     <section
@@ -25,6 +71,4 @@ export default function Gallery({
                     </section>
                 )
             }
-        </div>
-    )
-}
+        </div> */
