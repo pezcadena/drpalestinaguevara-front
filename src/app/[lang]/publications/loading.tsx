12762cc594e05f5c81ca6b82a49f3901b15f3ef8
Angel Cadena <angellopezcadena@gmail.com>
2024-05-22 08:21:46 -0600

    feat(Research): add loading skeleton

--- src/app/[lang]/publications/loading.tsx
+++ src/app/[lang]/publications/loading.tsx
@@ -5,14 +5,7 @@ export default function Loading() {
         <div className="flex flex-col gap-gap">
             <section className="flex gap-gap lg:h-[433px] flex-wrap lg:flex-nowrap flex-col-reverse lg:flex-row">
                 <article className="flex flex-col gap-gap p-padding justify-center bg-primary rounded h-full w-full lg:w-1/4">
-                    <div className="h-10 bg-gray-200 rounded w-1/2 animate-pulse"></div>
                     <div className="h-10 bg-gray-200 rounded w-full animate-pulse"></div>
-                    <div>
-                    <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
-                    <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
-                    <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
-                    <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
-                    </div>
                 </article>
                 <article className="flex flex-col gap-gap p-gap justify-center bg-primary animate-pulse rounded w-full h-[433px]">
                 </article>
--- src/app/[lang]/research/loading.tsx
+++ src/app/[lang]/research/loading.tsx
@@ -1,19 +1,80 @@
+import Separator from "../components/separator";
+
 export default function Loading() {
     return (
         <div className="flex flex-col gap-gap">
             <section className="flex gap-gap lg:h-[433px] flex-wrap lg:flex-nowrap flex-col-reverse lg:flex-row">
-                <article className="flex flex-col gap-gap p-padding justify-center bg-primary rounded h-full w-1/4">
-                    <div className="h-10 bg-gray-200 rounded w-1/2 animate-pulse"></div>
+                <article className="flex flex-col gap-gap p-padding justify-center bg-primary rounded h-full w-full lg:w-1/4">
                     <div className="h-10 bg-gray-200 rounded w-full animate-pulse"></div>
-                    <div>
-                    <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
-                    <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
-                    <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
-                    <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                </article>
+                <article className="flex flex-col gap-gap p-gap justify-center bg-primary animate-pulse rounded w-full h-[433px]">
+                </article>
+            </section>
+            <section className="
+                flex flex-col lg:flex-row-reverse relative gap-gap
+            ">
+                <article
+                    className="
+                    rounded
+                    flex
+                    overflow-hidden
+                    bg-secondary
+                    lg:flex-col
+                    lg:w-[15rem]
+                    lg:sticky 
+                    lg:top-gap
+                    h-fit
+                    "
+                >
+                    <div className="flex
+                                justify-center
+                                items-center
+                                px-padding
+                                py-gap
+                                ">
+                        <div className="h-5 bg-gray-200 rounded w-24 animate-pulse"></div>
+                    </div>
+                    <div className="flex
+                                justify-center
+                                items-center
+                                px-padding
+                                py-gap
+                                ">
+                        <div className="h-5 bg-gray-200 rounded w-24 animate-pulse"></div>
                     </div>
                 </article>
-                <article className="flex flex-col gap-gap p-gap justify-center bg-primary animate-pulse rounded w-full">
-                </article>
+                <div className="grow flex flex-col gap-gap">
+                    <article className="flex flex-col gap-gap justify-center bg-white rounded p-padding w-full">
+                            <div className="h-11 bg-primary rounded w-[20rem] animate-pulse"></div>
+                            <Separator/>
+                    </article>  
+                    <div className="flex gap-gap">
+                        <article className="flex flex-col gap-gap justify-center bg-white rounded grow">
+                            <div className="h-[20rem] w-full bg-primary rounded animate-pulse"></div>
+                            <div className="p-padding pt-gap">
+                                <div className="h-6 w-[15rem] bg-primary rounded animate-pulse"></div>
+                                <div className=" h-4 mt-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                                <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                                <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                                <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                                <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                                <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                            </div>
+                        </article>
+                        <article className="flex flex-col gap-gap justify-center bg-white rounded grow">
+                            <div className="h-[20rem] w-full bg-primary rounded animate-pulse"></div>
+                            <div className="p-padding pt-gap">
+                                <div className="h-6 w-[15rem] bg-primary rounded animate-pulse"></div>
+                                <div className=" h-4 mt-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                                <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                                <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                                <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                                <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                                <div className="mt-4 h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
             </section>
         </div>
     );
